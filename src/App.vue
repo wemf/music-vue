d653ad4aa7860e769e375b105208fe066963b1a3
Ondigital Backend Mac <ondigital.backend@gmail.com>
2018-05-28 17:04:09 -0500

    add selector for country

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,48 @@
   #app
     img(src='./assets/logo.png')
     h1 PlatziMusic
+    select(v-model="selectedCountry")
+      option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
     ul
-      li(v-for="artist in artists") {{ artist.name}}
+      artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
 </template>
 
 <script>
+import Artist from './components/Artist.vue'
 import getArtists from './api'
+
 export default {
   name: 'app',
   data () {
     return {
-      artists: []
+      artists: [],
+      countries: [
+        { name: 'Argentina', value: 'argentina'},
+        { name: 'Colombia', value: 'colombia'},
+        { name: 'España', value: 'spain'},
+      ],
+      selectedCountry: 'colombia'
     }
   },
-  mounted:  function () {
-    const self = this
-    getArtists()
-      .then(function (artists) {
-        self.artists = artists
-      })
+  components: {
+    Artist
+  },
+  methods: {
+    refreshArtists() {
+      const self = this
+      getArtists(this.selectedCountry)
+        .then(function (artists) {
+          self.artists = artists
+        })
+    }
+  },
+  mounted: function () {
+    this.refreshArtists()
+  },
+  watch: {
+    selectedCountry: function () {
+      this.refreshArtists()
+    }
   }
 }
 </script>
